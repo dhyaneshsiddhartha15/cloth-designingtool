d4db911afd7aabaf65950df67828b3a0bc754608
unknown <46subin@gmail.com>
2024-11-04 20:35:14 +0530

    default file open has been removed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     {
       _id: '123',
       name: 'Filename 1',
-      isOpen: true,
+      isOpen: false,
       file: `<?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 15.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
